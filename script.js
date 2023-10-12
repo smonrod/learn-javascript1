c3ff266b82002ad6ae7f7ed74e961054dba5e58d
Sebastián Monsalve Rodríguez <smonrod.92@gmail.com>
2023-10-12 17:34:19 -0500

    parte4.1: eventos dom

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const vendedor = {
-    nombre: 'Pedro',
-    apellido: 'Gonzalez',
-    empresa: 'Auto S.A',
-    habilidadesBlandas: ['Carisma', 'Puntualidad'],
-    vender: function(){
-        return "Pedro ha vendido un auto"
-    },
-    nombreCompleto: function(){
-        return this.nombre +" "+ this.apellido
-
-    }
+function mostrar(){
+    document.getElementById('seleccionable').innerHTML = Date()
 }
-
-//this se utiliza para llamar al atributo que esta fuera de su función o scope
-console.log(vendedor.nombreCompleto())
