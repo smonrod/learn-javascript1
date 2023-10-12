79894d49bdf2614ed29acc8b8896eefbb756bdce
Sebastián Monsalve Rodríguez <smonrod.92@gmail.com>
2023-10-12 15:11:01 -0500

    parte2.1: funciones

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.getElementById('demostracion').innerHTML = 'Este texto ha sido generado por JavaScript'
+
+function suma(x,y){
+    return x + y //return es lo que devuelve
+}
+
+let respuesta = suma(5,6)
+
+console.log(respuesta)
