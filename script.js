a051bf45791547cca925206da1b8198a86457cca
Sebastián Monsalve Rodríguez <smonrod.92@gmail.com>
2023-10-12 16:48:27 -0500

    parte3.2: objetos

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let auto1 = {
     peso: 550,
     color: 'Rojo'
 }
-
+//objeto:auto1 clave o atributo:color valor:rojo
 let auto2 = {
     marca: 'RENAULT',
     modelo: '12',
@@ -12,7 +12,5 @@ let auto2 = {
     color: 'Marrón'
 }
 
-console.log('Auto: ', auto1)
-console.log('------------------')
-console.log('Auto: ', auto2)
-console.log('------------------')
+console.log('El color del auto1 es: ', auto1.color)
+console.log('El color del auto2 es: ', auto2.color)
